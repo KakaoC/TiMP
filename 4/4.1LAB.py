8a1c973581ba0ad51d481ef80cc0a79cd16a20cc
MaestroFox <fiff906656@mail.ru>
2023-12-04 02:45:55 +0400

    4.1LAB

--- 4/4.1LAB.py
+++ 4/4.1LAB.py
@@ -1,20 +1,37 @@
-import os
- 
- 
-def longest_words(file):
-    with open(file, "r") as text:
-        words = text.read().split()
- 
-    max_length = len(max(words, key=len))
-    sought_words = [word for word in words if len(word) == max_length]
- 
-    f = "result.txt"
-    i = 1
-    while os.path.exists(f):
-        f = f"result{i}.txt"
-        i += 1
-    with open(f, 'w') as file:
-        file.write(' '.join(sought_words) + "\n")
- 
- 
-longest_words('article.txt')
+import tkinter as tk
+rainbow_colors = {
+    "красный": "#FF0000",
+    "оранжевый": "#FFA500",
+    "желтый": "#FFFF00",
+    "зеленый": "#008000",
+    "голубой": "#42AAFF",
+    "синий": "#0000FF",
+    "фиолетовый": "#8B00FF"
+}
+class RainbowColors: 
+    def __init__(self, master): 
+        self.master = master
+        self.master.title("Rainbow Colors") 
+        self.color_label = tk.Label(text="выберите цвет")
+        self.color_label.pack(pady=10) 
+        self.color_entry = tk.Entry(state="readonly") 
+        self.color_entry.pack(pady=10)
+        self.create_color_buttons() 
+    def create_color_buttons(self): 
+        for color_name, color_code in rainbow_colors.items(): 
+            button = tk.Button(text="\t\n\t", bg=color_code,command=lambda c=color_code, n=color_name: self.show_color(c,n))
+            button.pack(side=tk.LEFT)
+    def show_color(self, color_code,color_name): 
+        self.color_label.config(text=color_name) 
+        self.color_entry.config(state="normal") 
+        self.color_entry.delete(0, tk.END) 
+        self.color_entry.insert(0, color_code) 
+        self.color_entry.config(state="readonly") 
+
+def main():
+    window = tk.Tk()
+    app = RainbowColors(window)
+    window.resizable(width=False, height=False)
+    window.mainloop()
+if __name__ == "__main__":
+    main()
